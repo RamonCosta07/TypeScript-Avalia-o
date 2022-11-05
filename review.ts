6c04f7b11e47766d334a9b84cb7a291e2a865626
RamonCosta07 <ramonnar20@hotmail.com>
2022-11-05 00:57:07 -0300

    Programa simples com TypeScript utilizando Narrowing para imprimir nota de avaliação

---
+++ review.ts
@@ -0,0 +1,30 @@
+type Review = number | boolean;
+
+function review(avaliacao: Review) {
+    if (!avaliacao) {
+        console.log('Você não avaliou. Obrigado mesmo assim :(');
+        return;
+    }
+
+    if (typeof avaliacao === 'number') {
+        if (avaliacao > 0 && avaliacao <= 5) {
+            if (avaliacao === 1) {
+                console.log(`${avaliacao}: muito ruim`);
+            } else if (avaliacao === 2) {
+                console.log(`${avaliacao}: ruim`);
+            } else if (avaliacao === 3) {
+                console.log(`${avaliacao}: mediano`);
+            } else if (avaliacao === 4) {
+                console.log(`${avaliacao}: bom`);
+            } else {
+                console.log(`${avaliacao}: muito bom`);
+            }
+        } else {
+            console.log('Apenas notas de 1 à 5');
+        }
+    }
+}
+
+review(6);
+review(3);
+review(false);
